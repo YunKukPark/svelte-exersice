3637eaf9616e4e536e1d0b3b82d691c1a7f7b1a0
YunKukPark <a01043340999@gmail.com>
2022-01-25 14:54:10 +0900

    WIP: merge 하여 color가져오기 위한 commit

---
+++ src/lib/Login/Form.svelte
@@ -0,0 +1,7 @@
+<div>
+	<h2 class="text-center text-blue">Account Login</h2>
+	<form>
+		<input type="text" placeholder="Username: admin or user" />
+		<input type="password" placeholder="Password 를 입력해주세요" />
+	</form>
+</div>
---
+++ src/lib/Login/Header.svelte
@@ -0,0 +1,11 @@
+<script>
+	const logoUrl =
+		'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F2681c688-53f6-45b2-9397-2e9f4a7a6556%2Flogo-jenuio.png?table=block&id=77fc92a2-1a45-4b7d-bd7a-957300436b5f&spaceId=9300b0b5-831f-4b8f-9a3b-94ab6aaef8a4&width=2000&userId=bc464483-0118-41f1-a711-b573d17e8ba6&cache=v2';
+</script>
+
+<header class="flex justify-center items-center mb-5">
+	<div class="w-9 h-9 mr-3">
+		<img src={logoUrl} alt="제누이오 로고" />
+	</div>
+	<h1 class="uppercase text-2xl">genuio</h1>
+</header>
